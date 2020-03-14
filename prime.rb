08c6b008b36692f05a38111c14a7e8e9ac7a31ba
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 06:32:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
   def prime?(num)
+    if num < 0
+      return false
 
     (2..(num - 1)).each do |n|
       return false if num % n == 0
     end
     true
   end
-
-prime?(17)
