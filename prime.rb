7dbb92503929a71f8b52f9fb85793803bb64200b
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 06:37:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+
+  def prime?(num)
+    if num < 2
+      return false
+    end
+
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    true
+  end
